d321d6f9c35bb19501e2b9b0c7159d6a0ae70624
hugamana <45310627+hugamana@users.noreply.github.com>
2025-11-21 11:07:24 +0100

    Added jumping and ground layer check

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour
     
     [SerializeField] private float moveSpeed = 350f;
     [SerializeField] private float jumpForce = 700f;
+    [SerializeField] private Transform lFoot, rFoot;
+    [SerializeField] private LayerMask whatIsGround;
     
     private Rigidbody2D rgbd;
     private SpriteRenderer sprend;
@@ -15,6 +17,7 @@ public class PlayerMovement : MonoBehaviour
     
     private float horizontalValue;
     private float verticalValue;
+    private float rayDistance = 0.05f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -30,7 +33,9 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalValue = Input.GetAxis("Horizontal");
         
-        if(Input.GetKeyDown(KeyCode.Z) && isGrounded == true) {
+        CheckGrounded();
+        
+        if((Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown("space")) && isGrounded == true) {
             Jump();
         }
         
@@ -62,6 +67,25 @@ public class PlayerMovement : MonoBehaviour
     
     private void Jump() {
         rgbd.AddForce(new Vector2(0, jumpForce));
+    }
+
+    private void CheckGrounded()
+    {
+        RaycastHit2D lHit = Physics2D.Raycast(lFoot.position, Vector2.down, rayDistance, whatIsGround);
+        RaycastHit2D rHit = Physics2D.Raycast(rFoot.position, Vector2.down, rayDistance, whatIsGround);
+       
+        //Debug.DrawRay(lFoot.position, Vector2.down * rayDistance, Color.blue, 0.05f);
+        //Debug.DrawRay(rFoot.position, Vector2.down * rayDistance, Color.red, 0.05f);
+
+        if (lHit.collider != null && lHit.collider.CompareTag("Ground") ||
+            rHit.collider != null && rHit.collider.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
