4d8822733fca0d418f60305624096b221a7bc334
hugamana <45310627+hugamana@users.noreply.github.com>
2025-11-21 09:52:09 +0100

    Added main menu scene with scene change

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
